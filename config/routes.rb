11711ec598b9a5b2b6d3cbf3f32ee32be69da05a
Regina Jackson <rjackson156@gmail.com>
2021-12-28 19:51:56 -0500

    Update routes for app to work from /

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
+  get "/", to: "foods#index"
+  post "/", to: "foods#create"
   get "/foods", to: "foods#index"
   post "/foods", to: "foods#create"
 end
