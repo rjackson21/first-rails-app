ff6d1992b235d1795a62c9bdabccd0751eaf83ca
Regina Jackson <rjackson156@gmail.com>
2022-01-10 22:29:14 -0500

    Update routing to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get "/about", to: "pages#about"
-  get "/contact", to: "pages#contact"
-  get "/", to: "foods#index"
-  post "/", to: "foods#create"
-  get "/foods", to: "foods#index"
-  post "/foods", to: "foods#create"
+  resources :foods
 end
