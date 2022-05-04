597ba56e61e892e0c5450b1a13944df1501a674e
Regina Jackson <rjackson156@gmail.com>
2022-05-03 21:35:05 -0400

    drop friends table

---
+++ db/migrate/20220504012754_drop_friends.rb
@@ -0,0 +1,5 @@
+class DropFriends < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :friends
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211227230354) do
+ActiveRecord::Schema.define(version: 20220504012754) do
 
   create_table "foods", force: :cascade do |t|
     t.string  "name"
     t.integer "calories"
     t.boolean "is_vegan"
-  end
-
-  create_table "friends", force: :cascade do |t|
-    t.string   "first_name"
-    t.integer  "age"
-    t.integer  "years_of_friendship"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
   end
 
 end
