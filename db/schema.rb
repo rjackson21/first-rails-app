298a3f0d47db56d54bb4b06123578a0bae0a822e
Regina Jackson <rjackson156@gmail.com>
2022-05-05 19:40:53 -0400

    change from sqlite to postgres for deployment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20220504013922) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "foods", force: :cascade do |t|
     t.string  "name"
     t.boolean "is_vegan"
